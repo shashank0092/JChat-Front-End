d854b95c94b68ffcc333a3ea64c11ba26ffc3da7
SHASHANK <shashank7016204503@gmail.com>
2024-01-30 11:39:44 +0530

    Done

--- src/components/LoginGoogle.tsx
+++ src/components/LoginGoogle.tsx
@@ -8,6 +8,7 @@ const LoginGoogle=()=>{
     return(
         <>
             <button className="bg-red-300 p-5 px-14 flex gap-5 items-center rounded-full text-xl font-bold xsm:py-5 xsm:px-5" onClick={()=>loginWithRedirect()} > <FaGoogle/>  Google Login <FaArrowRight/> </button>
+            ;as;
         </>
     )
 }
